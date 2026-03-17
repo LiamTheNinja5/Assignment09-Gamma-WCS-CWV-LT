951b387d47545c0a2056bc1ea55824156a2f0ea5
WILLIAM SNELLER <SNELLERW@aquinas.edu>
2026-03-17 12:37:34 -0400

    init

---
+++ Assignment09-Gamma/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Assignment09_Gamma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pcbLogo = new System.Windows.Forms.PictureBox();
+            this.cmbBread = new System.Windows.Forms.ComboBox();
+            this.pnlBread = new System.Windows.Forms.Panel();
+            this.lblBread = new System.Windows.Forms.Label();
+            this.pnlFilling = new System.Windows.Forms.Panel();
+            this.lblFilling = new System.Windows.Forms.Label();
+            this.cmbFilling = new System.Windows.Forms.ComboBox();
+            this.pnlCondiments = new System.Windows.Forms.Panel();
+            this.cxbCondiments = new System.Windows.Forms.CheckedListBox();
+            this.lblCondiments = new System.Windows.Forms.Label();
+            this.pnlButtons = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).BeginInit();
+            this.pnlBread.SuspendLayout();
+            this.pnlFilling.SuspendLayout();
+            this.pnlCondiments.SuspendLayout();
+            this.pnlButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pcbLogo
+            // 
+            this.pcbLogo.BackColor = System.Drawing.Color.Transparent;
+            this.pcbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pcbLogo.Image")));
+            this.pcbLogo.Location = new System.Drawing.Point(186, 12);
+            this.pcbLogo.Name = "pcbLogo";
+            this.pcbLogo.Size = new System.Drawing.Size(256, 180);
+            this.pcbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbLogo.TabIndex = 0;
+            this.pcbLogo.TabStop = false;
+            // 
+            // cmbBread
+            // 
+            this.cmbBread.FormattingEnabled = true;
+            this.cmbBread.Location = new System.Drawing.Point(6, 33);
+            this.cmbBread.Name = "cmbBread";
+            this.cmbBread.Size = new System.Drawing.Size(216, 21);
+            this.cmbBread.TabIndex = 1;
+            // 
+            // pnlBread
+            // 
+            this.pnlBread.BackColor = System.Drawing.Color.Transparent;
+            this.pnlBread.Controls.Add(this.lblBread);
+            this.pnlBread.Controls.Add(this.cmbBread);
+            this.pnlBread.Location = new System.Drawing.Point(200, 207);
+            this.pnlBread.Name = "pnlBread";
+            this.pnlBread.Size = new System.Drawing.Size(228, 61);
+            this.pnlBread.TabIndex = 2;
+            // 
+            // lblBread
+            // 
+            this.lblBread.BackColor = System.Drawing.Color.Transparent;
+            this.lblBread.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBread.Location = new System.Drawing.Point(8, 7);
+            this.lblBread.Name = "lblBread";
+            this.lblBread.Size = new System.Drawing.Size(214, 23);
+            this.lblBread.TabIndex = 2;
+            this.lblBread.Text = "In Bread We Crust";
+            this.lblBread.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pnlFilling
+            // 
+            this.pnlFilling.BackColor = System.Drawing.Color.Transparent;
+            this.pnlFilling.Controls.Add(this.lblFilling);
+            this.pnlFilling.Controls.Add(this.cmbFilling);
+            this.pnlFilling.Location = new System.Drawing.Point(200, 303);
+            this.pnlFilling.Name = "pnlFilling";
+            this.pnlFilling.Size = new System.Drawing.Size(228, 61);
+            this.pnlFilling.TabIndex = 3;
+            // 
+            // lblFilling
+            // 
+            this.lblFilling.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFilling.Location = new System.Drawing.Point(8, 7);
+            this.lblFilling.Name = "lblFilling";
+            this.lblFilling.Size = new System.Drawing.Size(214, 23);
+            this.lblFilling.TabIndex = 2;
+            this.lblFilling.Text = "Fill it Up!";
+            this.lblFilling.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // cmbFilling
+            // 
+            this.cmbFilling.FormattingEnabled = true;
+            this.cmbFilling.Location = new System.Drawing.Point(6, 33);
+            this.cmbFilling.Name = "cmbFilling";
+            this.cmbFilling.Size = new System.Drawing.Size(216, 21);
+            this.cmbFilling.TabIndex = 1;
+            // 
+            // pnlCondiments
+            // 
+            this.pnlCondiments.BackColor = System.Drawing.Color.Transparent;
+            this.pnlCondiments.Controls.Add(this.cxbCondiments);
+            this.pnlCondiments.Controls.Add(this.lblCondiments);
+            this.pnlCondiments.Location = new System.Drawing.Point(200, 403);
+            this.pnlCondiments.Name = "pnlCondiments";
+            this.pnlCondiments.Size = new System.Drawing.Size(228, 186);
+            this.pnlCondiments.TabIndex = 4;
+            // 
+            // cxbCondiments
+            // 
+            this.cxbCondiments.FormattingEnabled = true;
+            this.cxbCondiments.Location = new System.Drawing.Point(6, 33);
+            this.cxbCondiments.Name = "cxbCondiments";
+            this.cxbCondiments.Size = new System.Drawing.Size(216, 139);
+            this.cxbCondiments.TabIndex = 3;
+            // 
+            // lblCondiments
+            // 
+            this.lblCondiments.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCondiments.Location = new System.Drawing.Point(8, 7);
+            this.lblCondiments.Name = "lblCondiments";
+            this.lblCondiments.Size = new System.Drawing.Size(214, 23);
+            this.lblCondiments.TabIndex = 2;
+            this.lblCondiments.Text = "Relax it\'s No Big Dill";
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.BackColor = System.Drawing.Color.Transparent;
+            this.pnlButtons.Controls.Add(this.btnExit);
+            this.pnlButtons.Controls.Add(this.btnSubmit);
+            this.pnlButtons.Location = new System.Drawing.Point(214, 606);
+            this.pnlButtons.Name = "pnlButtons";
+            this.pnlButtons.Size = new System.Drawing.Size(200, 49);
+            this.pnlButtons.TabIndex = 5;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(105, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(24, 12);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "&Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(629, 667);
+            this.Controls.Add(this.pnlButtons);
+            this.Controls.Add(this.pnlCondiments);
+            this.Controls.Add(this.pnlFilling);
+            this.Controls.Add(this.pnlBread);
+            this.Controls.Add(this.pcbLogo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).EndInit();
+            this.pnlBread.ResumeLayout(false);
+            this.pnlFilling.ResumeLayout(false);
+            this.pnlCondiments.ResumeLayout(false);
+            this.pnlButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbLogo;
+        private System.Windows.Forms.ComboBox cmbBread;
+        private System.Windows.Forms.Panel pnlBread;
+        private System.Windows.Forms.Label lblBread;
+        private System.Windows.Forms.Panel pnlFilling;
+        private System.Windows.Forms.Label lblFilling;
+        private System.Windows.Forms.ComboBox cmbFilling;
+        private System.Windows.Forms.Panel pnlCondiments;
+        private System.Windows.Forms.Label lblCondiments;
+        private System.Windows.Forms.CheckedListBox cxbCondiments;
+        private System.Windows.Forms.Panel pnlButtons;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ Assignment09-Gamma/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment09_Gamma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void standardFormSetup(Button btnAccept, Button btnCancel)
+        {
+            this.Location = new Point((Screen.PrimaryScreen.WorkingArea.Width - this.Width) / 2, (Screen.PrimaryScreen.WorkingArea.Height - this.Height) / 2);
+            this.MaximizeBox = false;
+            //this.Size = new Size(300,300)
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+            this.CancelButton = btnCancel;
+            this.AcceptButton = btnAccept;
+            this.Text = System.Reflection.Assembly.GetExecutingAssembly().EntryPoint.DeclaringType.Namespace + " - WCS - CWV - LT";
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
